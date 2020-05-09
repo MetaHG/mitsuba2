4c17790c1b5d9e2e38a4bce5b90a3393919d14d9
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-05-09 17:26:36 +0200

    Clean path integrator (AOV still present)

--- src/integrators/path.cpp
+++ src/integrators/path.cpp
@@ -108,7 +108,6 @@ public:
         m_aov_names.push_back("pure.A");
 
         m_aov_types.push_back(Type::IntegratorRGBA);
-        // Check here if I have to add another name before for integrator (AOV line 120)
         m_aov_names.push_back("path.R");
         m_aov_names.push_back("path.G");
         m_aov_names.push_back("path.B");
@@ -130,6 +129,7 @@ public:
             switch (m_aov_types[i]) {
                 case Type::Pure: {
                     std::pair<Spectrum, Mask> result_sub = test_path_sample(scene, sampler, ray, aovs, active);
+                    std::pair<Spectrum, Mask> result_sub = pure_path_sample(scene, sampler, ray, aovs, active);
 
                     UnpolarizedSpectrum spec_u = depolarize(result_sub.first);
 
@@ -184,7 +184,7 @@ public:
         return result;
     }
 
-    std::pair<Spectrum, Mask> test_path_sample(const Scene *scene,
+    std::pair<Spectrum, Mask> pure_path_sample(const Scene *scene,
                                      Sampler *sampler,
                                      const RayDifferential3f &ray_,
                                      Float * /* aovs */,
@@ -209,120 +209,6 @@ public:
         result[active_e] += emitter_val;
 
         // ----------------------- BSDF sampling ----------------------
-
-        return { result, valid_ray };
-    }
-
-    std::pair<Spectrum, Mask> pure_path_sample(const Scene *scene,
-                                     Sampler *sampler,
-                                     const RayDifferential3f &ray_,
-                                     Float * /*aovs*/,
-                                     Mask active) const {
-        MTS_MASKED_FUNCTION(ProfilerPhase::SamplingIntegratorSample, active);
-
-        RayDifferential3f ray = ray_;
-
-        // Tracks radiance scaling due to index of refraction changes
-        Float eta(1.f);
-
-        // MIS weight for intersected emitters (set by prev. iteration)
-        Float emission_weight(1.f);
-
-        Spectrum throughput(1.f), result(0.f);
-
-        // ---------------------- First intersection ----------------------
-
-        SurfaceInteraction3f si = scene->ray_intersect(ray, active);
-        Mask valid_ray = si.is_valid();
-        EmitterPtr emitter = si.emitter(scene);
-
-        for (int depth = 1;; ++depth) {
-
-            // ---------------- Intersection with emitters ----------------
-
-            if (any_or<true>(neq(emitter, nullptr))) {
-                result[active] += emission_weight * throughput * emitter->eval(si, active);
-            }
-
-            active &= si.is_valid();
-
-            /* Russian roulette: try to keep path weights equal to one,
-               while accounting for the solid angle compression at refractive
-               index boundaries. Stop with at least some probability to avoid
-               getting stuck (e.g. due to total internal reflection) */
-            if (depth > m_rr_depth) {
-                Float q = min(hmax(depolarize(throughput)) * sqr(eta), .95f);
-                active &= sampler->next_1d(active) < q;
-                throughput *= rcp(q);
-            }
-
-            // Stop if we've exceeded the number of requested bounces, or
-            // if there are no more active lanes. Only do this latter check
-            // in GPU mode when the number of requested bounces is infinite
-            // since it causes a costly synchronization.
-            if ((uint32_t) depth >= (uint32_t) m_max_depth ||
-                ((!is_cuda_array_v<Float> || m_max_depth < 0) && none(active)))
-                break;
-
-            // --------------------- Emitter sampling ---------------------
-
-            BSDFContext ctx;
-            BSDFPtr bsdf = si.bsdf(ray);
-            Mask active_e = active && has_flag(bsdf->flags(), BSDFFlags::Smooth);
-
-            if (likely(any_or<true>(active_e))) {
-                auto [ds, emitter_val] = scene->sample_emitter_direction_pure(
-                    si, sampler->next_3d(active_e), true, active_e);
-                active_e &= neq(ds.pdf, 0.f);
-
-                // Query the BSDF for that emitter-sampled direction
-                Vector3f wo = si.to_local(ds.d);
-                Spectrum bsdf_val = bsdf->eval(ctx, si, wo, active_e);
-                bsdf_val = si.to_world_mueller(bsdf_val, -wo, si.wi);
-
-                // Determine density of sampling that same direction using BSDF sampling
-                Float bsdf_pdf = bsdf->pdf(ctx, si, wo, active_e);
-
-                Float mis = select(ds.delta, 1.f, mis_weight(ds.pdf, bsdf_pdf));
-                result[active_e] += mis * throughput * bsdf_val * emitter_val;
-            }
-
-            // ----------------------- BSDF sampling ----------------------
-
-            // Sample BSDF * cos(theta)
-            auto [bs, bsdf_val] = bsdf->sample(ctx, si, sampler->next_1d(active),
-                                               sampler->next_2d(active), active);
-            bsdf_val = si.to_world_mueller(bsdf_val, -bs.wo, si.wi);
-
-            throughput = throughput * bsdf_val;
-            active &= any(neq(depolarize(throughput), 0.f));
-            if (none_or<false>(active))
-                break;
-
-            eta *= bs.eta;
-
-            // Intersect the BSDF ray against the scene geometry
-            ray = si.spawn_ray(si.to_world(bs.wo));
-            SurfaceInteraction3f si_bsdf = scene->ray_intersect(ray, active);
-            Mask active_b = active && si_bsdf.is_valid();
-
-            /* Determine probability of having sampled that same
-               direction using emitter sampling. */
-            emitter = si_bsdf.emitter(scene, active_b);
-            DirectionSample3f ds(si_bsdf, si);
-            ds.object = emitter;
-
-            if (any_or<true>(neq(emitter, nullptr))) {
-                Float emitter_pdf =
-                    select(neq(emitter, nullptr) && !has_flag(bs.sampled_type, BSDFFlags::Delta),
-                           scene->pdf_emitter_direction_pure(si, ds, active_b),
-                           0.f);
-
-                emission_weight = mis_weight(bs.pdf, emitter_pdf);
-            }
-
-            si = std::move(si_bsdf);
-        }
 
         return { result, valid_ray };
     }
